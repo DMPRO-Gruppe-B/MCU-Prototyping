63fd07fd2209b8b4f960f2b137320f2fe207f06d
Aksel Slettemark <akselslettemark@gmail.com>
2019-10-22 02:05:23 +0200

    Define struct for individual settings. Generalize sending bytes over SPI.

--- STK3700_SPIMaster/src/main.c
+++ STK3700_SPIMaster/src/main.c
@@ -1,5 +1,5 @@
 #include <string.h>
-//#include <stdint.h>
+#include <stdint.h>
 #include <stdbool.h>
 //#include "em_device.h"
 #include "em_chip.h"
@@ -12,11 +12,45 @@
 
 #define BIT(x) (1U << (x))
 
+typedef unsigned char byte;
+
+typedef struct {
+	uint8_t id;
+	char *name;
+	char *unit;
+	uint16_t step_size;
+	uint16_t value;
+} setting_t;
+
+void send_byte(byte b) {
+	for (int j = 0; j < 8; j++) {
+		if (b & BIT(7 - j)) {
+			GPIO_PinOutSet(gpioPortD, 5);
+		} else {
+			GPIO_PinOutClear(gpioPortD, 5);
+		}
+		Delay(2);
+		GPIO_PinOutSet(gpioPortD, 6);
+		Delay(2);
+		GPIO_PinOutClear(gpioPortD, 6);
+	}
+}
+
+void send_setting(setting_t *setting) {
+	byte bytes[3] = { setting->id, (setting->value & 0xFF00) >> 8,
+			setting->value & 0xFF };
+	for (int i = 0; i < 3; i++) {
+		byte b = bytes[i];
+		send_byte(b);
+	}
+}
+
 void GPIO_Init() {
 	CMU_ClockEnable(cmuClock_GPIO, true);
 
 	for (int i = 0; i <= 6; i++) {
 		GPIO_PinModeSet(gpioPortD, i, gpioModePushPull, 0);
+		GPIO_PinOutClear(gpioPortD, i);
 	}
 }
 
@@ -32,35 +66,42 @@ int main(void) {
 	}
 
 	GPIO_Init();
+	Delay(20);
 
-	char *yeeters = "Hello from EFM32\nlmao yeet\n";
-	for (int i = 0; i < strlen(yeeters); i++) {
-		char c = yeeters[i];
-		for (int j = 0; j < 8; j++) {
-			GPIO_PinOutClear(gpioPortD, 6);
-			Delay(2);
-			GPIO_PinOutSet(gpioPortD, 6);
+	/*
+	 char *yeeters = "Hello from EFM32\nlmao yeet\n";
+	 for (int i = 0; i < strlen(yeeters); i++) {
+	 char c = yeeters[i];
+	 for (int j = 0; j < 8; j++) {
+	 GPIO_PinOutClear(gpioPortD, 6);
+	 Delay(2);
+	 GPIO_PinOutSet(gpioPortD, 6);
 
-			if (c & BIT(7 - j)) {
-				GPIO_PinOutSet(gpioPortD, 5);
-			} else {
-				GPIO_PinOutClear(gpioPortD, 5);
-			}
-			Delay(2);
-		}
-	}
+	 if (c & BIT(7 - j)) {
+	 GPIO_PinOutSet(gpioPortD, 5);
+	 } else {
+	 GPIO_PinOutClear(gpioPortD, 5);
+	 }
+	 Delay(2);
+	 }
+	 }
+	 */
 
-	int yeet = 0;
-	while (1) {
-		for (int i = 0; i < 5; i++) {
-			if (yeet & BIT(i)) {
-				GPIO_PinOutSet(gpioPortD, i);
+	/*
+	 int yeet = 0;
+	 while (1) {
+	 for (int i = 0; i < 5; i++) {
+	 if (yeet & BIT(i)) {
+	 GPIO_PinOutSet(gpioPortD, i);
 
-			} else {
-				GPIO_PinOutClear(gpioPortD, i);
-			}
-		}
-		yeet++;
-		Delay(10);
-	}
+	 } else {
+	 GPIO_PinOutClear(gpioPortD, i);
+	 }
+	 }
+	 yeet++;
+	 Delay(10);
+	 }
+	 */
+	setting_t test1 = { .id = 1, .name = "oof", .unit = "ms", .value = 10 };
+	send_setting(&test1);
 }
